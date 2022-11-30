6fe7464fff103539141eabaaba19795692297b8b
Usek777 <91222585+Usek777@users.noreply.github.com>
2022-11-30 05:48:47 +0300

    BagFix and more
    Пофиксил баги прошлого патча. Добавил возможность ускорять персонажа на Lshift и замедлять на Lcontrol. Добавил более резкое торможение на ускорении (с возможностью регуляции). Немного исправил ситуацию с поворотом персонажа.

--- Assets/Scripts/PlayerScripts/Warrior/WarriorMovement.cs
+++ Assets/Scripts/PlayerScripts/Warrior/WarriorMovement.cs
@@ -6,19 +6,22 @@ using UnityEngine;
 
 public class WarriorMovement : MonoBehaviour
 {
+
     //140 градусов на доворот
-    private const float WarriorRotateCorrection = 140f;
+    private const float WarriorRotateCorrection = 128f;
 
     //Знак направления
     private sbyte DirectionSignX = 0;
     private sbyte DirectionSignY = 0;
 
-    //Тормоза
+    //Полная остановка при достижении этой скорости, если не растет скорость
     private const double Brakes = 0.2;
-    private const sbyte MaxSpeed = 1;
+    //Максимальная скорость
+    private float MaxSpeed = 0.7f;
+    //Коэффициент торможения
+    private const float BrakeFactor = 6f;
 
     //Компоненты персонажа
-    [SerializeField]
     private float WarriorSpeed = 1f;
     [SerializeField]
     private Rigidbody2D Warrior;
@@ -29,6 +32,48 @@ public class WarriorMovement : MonoBehaviour
     Vector2 MovementDirection;
     Vector2 MousePosition;
 
+
+
+
+
+    //Скорость по оси X
+    private float SpeedOnX() => Warrior.velocity.x;
+
+    //Скорость по оси Y
+    private float SpeedOnY() => Warrior.velocity.y;
+
+    private void ReverseImpulseX(float Speed)
+    {
+        Warrior.AddForce(new Vector2(DirectionSignX * (-1), 0) * Speed, ForceMode2D.Impulse);
+        if (Mathf.Abs(SpeedOnX()) <= Brakes) Warrior.velocity = new Vector2(0, SpeedOnY());
+    }
+        
+
+    private void ReverseImpulseY(float Speed)
+    {
+        Warrior.AddForce(new Vector2(0, DirectionSignY * (-1)) * Speed, ForceMode2D.Impulse);
+        if (Mathf.Abs(SpeedOnY()) <= Brakes) Warrior.velocity = new Vector2(SpeedOnX(), 0);
+    }
+
+    private void ReverseImpulseX(float Speed, float BrakeSpeed)
+    {
+        Warrior.AddForce(new Vector2(DirectionSignX * (-1), 0) * Speed * BrakeSpeed , ForceMode2D.Impulse);
+        if (Mathf.Abs(SpeedOnX()) <= Brakes) Warrior.velocity = new Vector2(0, SpeedOnY());
+    }
+
+    private void ReverseImpulseY(float Speed, float BrakeSpeed)
+    {
+        Warrior.AddForce(new Vector2(0, DirectionSignY * (-1)) * Speed * BrakeSpeed, ForceMode2D.Impulse);
+        if (Mathf.Abs(SpeedOnY()) <= Brakes) Warrior.velocity = new Vector2(SpeedOnX(), 0);
+    }
+
+
+
+
+
+
+
+
     void Update()
     {
         //Направлкние движения
@@ -36,47 +81,86 @@ public class WarriorMovement : MonoBehaviour
         MovementDirection.y = Input.GetAxisRaw("Vertical");
         //Позиция курсора
         MousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        //Замедлить или ускорить перса на shift или control
+        if (Input.GetKey(KeyCode.LeftShift)) MaxSpeed = 1;
+        else if (Input.GetKey(KeyCode.LeftControl)) MaxSpeed = 0.5f;
+        else MaxSpeed = 0.7f;
     }
 
     private void FixedUpdate()
     {
+
         //Поворот персонажа
         Vector2 LookDirection = MousePosition - Warrior.position;
         Warrior.rotation = (Mathf.Atan2(LookDirection.x, LookDirection.y) * -Mathf.Rad2Deg) + WarriorRotateCorrection;
 
 
 
-        //Ускорение (импульс) игрока по оси Х
-        if ( Mathf.Abs(Warrior.velocity.x) < MaxSpeed) Warrior.AddForce(new Vector2(MovementDirection.x, 0) * WarriorSpeed, ForceMode2D.Impulse);
-        else Warrior.velocity = new Vector2(MaxSpeed * DirectionSignX, Warrior.velocity.y);
 
-        //Ускорение (импульс) игрока по оси Y
-        if ( Mathf.Abs(Warrior.velocity.y) < MaxSpeed) Warrior.AddForce(new Vector2(0, MovementDirection.y) * WarriorSpeed, ForceMode2D.Impulse);
-        else Warrior.velocity = new Vector2(Warrior.velocity.x, MaxSpeed * DirectionSignY);
+        //Ускорение (импульс) игрока по оси Х в пределах максимальной скорости
+        if ( Mathf.Abs( SpeedOnX() ) < MaxSpeed) Warrior.AddForce(new Vector2(MovementDirection.x, 0) * WarriorSpeed, ForceMode2D.Impulse);
+        else Warrior.velocity = new Vector2(MaxSpeed * DirectionSignX, SpeedOnY());
+
+        //Ускорение (импульс) игрока по оси Y в пределах максимальной скорости
+        if ( Mathf.Abs( SpeedOnY() ) < MaxSpeed) Warrior.AddForce(new Vector2(0, MovementDirection.y) * WarriorSpeed, ForceMode2D.Impulse);
+        else Warrior.velocity = new Vector2(SpeedOnX(), MaxSpeed * DirectionSignY);
 
 
-        //Обратное ускорение (импульс) по Х если не зажаты кнопки
-        if (Warrior.velocity.x > 0) this.DirectionSignX = 1;
 
-        else if (Warrior.velocity.x < 0) this.DirectionSignX = -1;
 
-        if ((MovementDirection.x == 0) & ( (Warrior.velocity.x != 0) ))
+
+
+        /*Если направление по X (Key A, D) отличается от фактического направления движения,
+         * то перс тормозится обратным импульсом, который, как правило, больше обратного импульса при отпущенных кнопках.
+         * Импульс становиться больше, посредством умножения скорости на коэффициент торможения
+         */
+        if (DirectionSignX != MovementDirection.x)
         {
-            Warrior.AddForce(new Vector2( DirectionSignX * (-1), 0) * WarriorSpeed, ForceMode2D.Impulse);
-            if (Mathf.Abs(Warrior.velocity.x) <= Brakes) Warrior.velocity = new Vector2(0, Warrior.velocity.y);
+            if ( ( MovementDirection.x != 0 ) & ( DirectionSignX != 0 ) )
+            {
+                ReverseImpulseX(WarriorSpeed, BrakeFactor);
+                DirectionSignX = 0;
+            }
+        }
+
+        /*Если направление по Y (Key W, S) отличается от фактического направления движения,
+         * то перс тормозится обратным импульсом, который, как правило, больше обратного импульса при отпущенных кнопках.
+         * Импульс становиться больше, посредством умножения скорости на коэффициент торможения
+         */
+        if (DirectionSignY != MovementDirection.y)
+        {
+            if ( (MovementDirection.y != 0) & (DirectionSignY != 0) )
+            {
+                ReverseImpulseY(WarriorSpeed, BrakeFactor);
+                DirectionSignY = 0;
+            }
         }
 
 
 
-        //Обратное ускорение (импульс) по Y если не зажаты кнопки
-        if (Warrior.velocity.y > 0) this.DirectionSignY = 1;
 
-        else if (Warrior.velocity.y < 0) this.DirectionSignY = -1;
 
-        if ((MovementDirection.y == 0) & ((Warrior.velocity.y != 0)))
+        //Торможение персонажа по оси Х если не зажаты кнопки
+        if (SpeedOnX() > 0) this.DirectionSignX = 1;
+
+        else if (SpeedOnX() < 0) this.DirectionSignX = -1;
+
+        if ((MovementDirection.x == 0) & ( (SpeedOnX() != 0) ))
         {
-            Warrior.AddForce(new Vector2(0, DirectionSignY * (-1)) * WarriorSpeed, ForceMode2D.Impulse);
-            if (Mathf.Abs(Warrior.velocity.y) <= Brakes) Warrior.velocity = new Vector2(Warrior.velocity.x, 0);
+            ReverseImpulseX(WarriorSpeed);
+        }
+
+
+
+        //Торможение персонажа по оси Y если не зажаты кнопки
+        if (SpeedOnY() > 0) this.DirectionSignY = 1;
+
+        else if (SpeedOnY() < 0) this.DirectionSignY = -1;
+
+        if ((MovementDirection.y == 0) & ((SpeedOnY() != 0)))
+        {
+            ReverseImpulseY(WarriorSpeed);
         }
 
     }
