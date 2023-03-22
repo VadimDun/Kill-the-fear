a3fc4237d6df38eb953ad6b89329b7ceca957a9f
nktfmnko <nktfmnko@gmail.com>
2023-03-22 19:46:49 +0300

    EscPause

--- Assets/Scripts/Windows/PauseMenu.cs
+++ Assets/Scripts/Windows/PauseMenu.cs
@@ -24,4 +24,12 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene(sceneID);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
 }
