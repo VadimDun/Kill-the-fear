f99be7ab306fab14d700094fef3e959f7465894c
Usek777 <91222585+Usek777@users.noreply.github.com>
2023-03-22 13:54:25 +0300

    Add scenes
    Add empty scenes on the others buttons

--- Assets/Scripts/Windows/SelectLevelsScript.cs
+++ Assets/Scripts/Windows/SelectLevelsScript.cs
@@ -10,6 +10,21 @@ public class SelectLevelsScript : MonoBehaviour
         SceneManager.LoadScene(2);
     }
 
+    public void Level2() 
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(5);
+    }
+
     public void Back()
     {
         SceneManager.LoadScene(0);
